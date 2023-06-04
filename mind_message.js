593e961c56ed96d2394e6e7ccc428dbff7a684e0
Julian T <julian.thiel@outlook.com>
2023-06-04 17:41:56 +0200

    Add function for random astrology message

--- mind_message.js
+++ mind_message.js
@@ -1,6 +1,28 @@
-console.log("Does this thing even work");
+// Arrays with message fragments
 
-console.log("So I can add things in GitHub?");
+/*
+// Array of star sign not needed anymore. Message will now require input of user
+const starSigns = ["Aquarius", "Pisces", "Aries", "Taurus", "Gemini", "Cancer", "Leo", "Virgo", "Libra", "Scorpio", "Sagittarius", "Capricorn"];
+*/
 
-console.log("This is some VS stuff right here");
+const msg1 = ["be having good luck today", "experiencing traumatic flashbacks", "have a great day today", "not fulfill your goals today", "face challenges", "face danger", "have challenging emotions"];
+
+const msg2 = ["avoid acidy food", "pay attention to your fellow humans", "not go outside today", "be extra friendly today", "be on your guard", "not worry at all"];
+
+const giveMeASign = (starSign) => {
+    let randNum1 = Math.floor(Math.random()*7);
+    let optMsg1 = msg1[randNum1]
+
+    let randNum2 = Math.floor(Math.random()*7);
+    let optMsg2 = msg2[randNum2];
+
+    console.log(`You told me that your sign is ${starSign}!`);
+    console.log(`You will ${optMsg1}.`);
+    console.log(`You should ${optMsg2}.`);
+
+};
+
+
+// The star sign needs to be the argument of the function
+giveMeASign("Aquarius");
 
